a994daa1187d27b294b82ae99f6bd31cd44a3bff
Shane Cummings <shanecummings_uk@yahoo.co.uk>
2026-02-23 18:12:52 +0000

    feat: refactor todo hooks to use todoService for data fetching

--- src/React-Query/hooks/useTodos.ts
+++ src/React-Query/hooks/useTodos.ts
@@ -1,22 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient from "../services/apiClient";
 import { CACHE_KEY_TODOS } from "../constants";
+import todoService, { type Todo } from "../services/todoService";
 
-const apiClient = new APIClient<Todo>("/todos");
 
-export interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
 
 const useTodos = () => {
 
-
   return useQuery<Todo[], Error>({
     queryKey: CACHE_KEY_TODOS,
-    queryFn: apiClient.getAll.bind(apiClient),
+    queryFn: todoService.getAll,
     staleTime: 10 * 1000, // 10Sec - this is the time in milliseconds that the data will be considered fresh. During this time, the data will not be refetched when the component mounts or when the window is refocused. This can help to improve performance and reduce unnecessary network requests.
   });
 };
